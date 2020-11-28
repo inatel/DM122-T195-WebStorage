40f881033c92440014fd54cf937af011591e961e
Edy Segura <edysegura@gmail.com>
2020-11-28 12:31:58 -0300

    Implement IndexedDB
    - This replaces localStorage

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import {
+  set as setItem,
+  get as getItem,
+  keys as getKeys,
+} from "https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval.mjs";
 class App {
   constructor() {
     console.log("Initialized");
@@ -17,25 +22,26 @@ class App {
     const valueInput = document.getElementById("value");
 
     if (keyInput.value && valueInput.value) {
-      localStorage.setItem(keyInput.value, valueInput.value);
-      this.listStorageValues();
+      setItem(keyInput.value, valueInput.value)
+        .then(() => {
+          this.listStorageValues();
+        });
     }
   }
 
-  listStorageValues() {
+  async listStorageValues() {
     const storageValues = document.getElementById("storageValues");
 
-    const toHtml = (key) => {
-      const value = localStorage.getItem(key);
+    const toHtml = async (key) => {
+      const value = await getItem(key);
+      console.log(value);
       return `<p>${key}: ${value}</p>`;
     };
 
-    const htmlOutput = Object.keys(localStorage)
-      .sort()
-      .map(toHtml)
-      .join("");
+    const keys = await getKeys();
+    const htmlOutput = await Promise.all(keys.map(toHtml));
 
-    storageValues.innerHTML = htmlOutput;
+    storageValues.innerHTML = htmlOutput.join('');
   }
 }
 

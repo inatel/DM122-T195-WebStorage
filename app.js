b6592818d8324bf04cbdded7e23617e5a84b0919
Edy Segura <edysegura@gmail.com>
2020-11-28 10:59:41 -0300

    Implement saveToStorage method

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 class App {
   constructor() {
     console.log("Initialized");
+    this.bindButtonListener();
+  }
+
+  bindButtonListener() {
+    const button = document.querySelector("button");
+    button.addEventListener("click", () => {
+      this.saveToStorage();
+    });
+  }
+
+  saveToStorage() {
+    const keyInput = document.getElementById("key");
+    const valueInput = document.getElementById("value");
+
+    if (keyInput.value && valueInput.value) {
+      localStorage.setItem(keyInput.value, valueInput.value);
+    }
   }
 }
 
